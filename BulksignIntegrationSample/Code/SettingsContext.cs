0ba2d56ba27ab334e336417ceb7f515bcaad2552
Bulksign <contact@bulksign.com>
2020-08-11 16:23:01 +0300

    integration update, handle all callbacks

---
+++ BulksignIntegrationSample/Code/SettingsContext.cs
@@ -0,0 +1,24 @@
+﻿namespace BulksignIntegrationSample.Code
+{
+	public static class SettingsContext
+	{
+
+		public static string DatabaseConnectionString	  
+		{
+			get;
+			set;
+		}
+
+		public static string BulksignRootApiUrl
+		{
+			get;
+			set;
+		}
+
+
+		public static string BulksignSoapUrl => BulksignRootApiUrl + "/Bulksignapi.asmx";
+
+		public static string BulksignRestUrl => BulksignRootApiUrl + "/v1/";
+
+	}
+}
--- BulksignIntegrationSample/Global.asax.cs
+++ BulksignIntegrationSample/Global.asax.cs
@@ -1,23 +1,41 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using SettingsContext = BulksignIntegrationSample.Code.SettingsContext;
 
 namespace BulksignIntegrationSample
 {
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-    }
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			
+			SettingsContext.DatabaseConnectionString = ConfigurationManager.AppSettings["IntegrationDatabaseConnectionString"];
+			SettingsContext.BulksignRootApiUrl = ConfigurationManager.AppSettings["BulksignRootApiUrl"];
+
+
+			if (string.IsNullOrWhiteSpace(SettingsContext.DatabaseConnectionString))
+			{
+				throw new ConfigurationErrorsException("The value for 'DatabaseConnectionString' is empty");
+			}
+
+
+			if (string.IsNullOrWhiteSpace(SettingsContext.BulksignRootApiUrl))
+			{
+				throw new ConfigurationErrorsException("The value for 'BulksignRootApiUrl' is empty");
+			}
+
+
+		}
+	}
 }
